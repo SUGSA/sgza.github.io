6fd7a8a4536cdc469ce7a687c7f3b5bda0d32fe1
Barry Tandy <barry.tandy@gmail.com>
2019-06-05 20:12:54 +0200

    correct date for the countdown timer

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("17 October 2019 7:00:00 GMT+02:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
